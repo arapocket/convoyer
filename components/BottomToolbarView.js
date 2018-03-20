a4fbafaeb8a3d989a558ca1bc31dd6d8801e117e
arapocket <arakazaryan@me.com>
2018-03-20 12:53:36 -0700

    fixed flex for bottom toolbar

--- components/BottomToolbarView.js
+++ components/BottomToolbarView.js
@@ -221,7 +221,7 @@ var styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    flex: 0.1
+    flex: 0.125
 
   },
   btnNavigate: {
